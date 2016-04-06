5a68706ae320c01b4df55e9c7ad15f27cfc1c73d
Oleksandra Puravets <o.puravets@levi9.com>
2016-04-06 10:19:54 +0300

    added list of cats and updated onclick behavior

--- script.js
+++ script.js
@@ -1,41 +1,57 @@
-$(document).ready(function () {
-	
-
-	var timesClicked = 0; 
-	var timesWord = "";
-	var clickCount; 
-
-    $('.main-pic').on('click', function() {
-    	timesClicked++;
-    	console.log(timesClicked);
-
-    	if(timesClicked==1) {
-    		timesWord = 'time';
-    	} else {
-    		timesWord = 'times';    	}
+$(document).ready(function() {
 
 
-    $('.main-text').html('I was clicked ' + timesClicked + ' ' + timesWord);
+    var timesClicked = 0;
+    var timesWord = "";
+    var clicksCount = {};
+    var name;
+
+    function setClicksCount(name) {
+        if (clicksCount[name] == undefined) {
+            clicksCount[name] = 1
+        } else {
+            clicksCount[name]++;
+        }
+
+        return clicksCount[name]
+
+    }
+
+    function updateMessage(clickCount) {
+        if (clickCount == 1) {
+            timesWord = 'time';
+        } else {
+            timesWord = 'times';
+        }
+
+        $('.cat-info').find('.message').html('I was clicked ' + clickCount + ' ' + timesWord);
+    }
+
+
+
+    $('.cats').on('click', 'li', function(e) {
+        var currentCat = $(this);
+        name = currentCat.data('catName');
+        var clickCount = setClicksCount(name);
+        console.log(clickCount);
+
+
+        $('.cat-info').find('.name').html(name);
+        $('.cat-info').find('.cat-image').attr('src', currentCat.data('pictureSource'));
+
+        updateMessage(clickCount);
 
     });
 
-     $('.cat').on('click', 'img', function(e) {
-    	var currentImg = $(this);
 
-        clickCount = currentImg.data('clickCount');
-        console.log(clickCount);
+    $('.cat-info').on('click', 'img', function(e) {
 
-    	currentImg.data('clickCount', ++clickCount);
-    	console.log(clickCount);
+        var currentImg = $(this);
+        var clickCount = setClicksCount(name);
+        updateMessage(clickCount);
 
 
-    	currentImg.prev().html('I was clicked ' + timesClicked + ' ' + timesWord);
-    })   
-
-
-
-
-
+    });
 
 
 })
